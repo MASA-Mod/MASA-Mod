4bc58005604b20eddb84e6200d486f271afdf80e
Lukas Neumann <63599599+neumann-lukas@users.noreply.github.com>
2021-09-26 11:36:07 +0100

    Add files via upload

--- src/main/java/tk/masa/setup/ClientSetup.java
+++ src/main/java/tk/masa/setup/ClientSetup.java
@@ -5,6 +5,7 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import tk.masa.masa;
+import tk.masa.tileentitys.compressor.BlockCompressorConfigScreen;
 import tk.masa.tileentitys.compressor.BlockCompressorScreen;
 import tk.masa.tileentitys.ironfurnace.BlockIronFurnaceScreen;
 
@@ -14,6 +15,7 @@ public class ClientSetup {
     public static void init(final FMLClientSetupEvent event) {
         ScreenManager.registerFactory(Registration.IRON_FURNACE_CONTAINER.get(), BlockIronFurnaceScreen::new);
         ScreenManager.registerFactory(Registration.COMPRESSOR_CONTAINER.get(), BlockCompressorScreen::new);
+        //ScreenManager.registerFactory(, BlockCompressorConfigScreen::new);
     }
 
 }
